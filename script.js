b1bb13cf7962db06a0b8e07c7ba6c15913d4fa79
GabriellavdK <81296778+GabriellavdK@users.noreply.github.com>
2021-05-14 22:38:47 +0200

    Opdracht 22 geupload

---
+++ script.js
@@ -0,0 +1,33 @@
+const age = 32;
+const isFemale = true;
+const driverStatus = "bob";
+
+if (age >= 18) {
+
+    console.log("Welkom binnen!")
+
+} else {
+
+    console.log("Helaas ben je nog niet oud genoeg om naar binnen te gaan. Wanneer je 18 bent, ben je van harte welkom.")
+
+}
+
+if (isFemale) {
+
+    console.log("Welkom op onze Ladies Night!")
+
+} else {
+
+    console.log("Sorry maar op Ladies Night zijn mannen niet welkom. Boek gerust een andere datum wanneer er geen Ladies evenement op de planning staat.")
+
+}
+
+if (driverStatus) {
+
+    console.log("Yes jij bent de Bob vanavond, jij mag achter het stuur kruipen!")
+
+} else {
+
+    console.log("Helaas heb je teveel gedronken en kan ik het niet toelaten dat je achter het stuur stapt.")
+
+}
